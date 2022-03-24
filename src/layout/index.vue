ac9aac1b8ccc056fffebbc4c5c5922e19ccbf00a
zss <1740308215@qq.com>
2022-03-24 18:05:53 +0800

    添加路由和UI库

---
+++ src/layout/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="240px">
+          <sidemenu></sidemenu>
+        </el-aside>
+        <el-container>
+          <el-main>
+            <router-view></router-view>
+          </el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import sidemenu from './sidemenu.vue'
+
+export default {
+  components: { sidemenu },
+}
+</script>
+
+<style lang="scss">
+.common-layout {
+  height: 100%;
+}
+.el-aside {
+  // background-color: blue;
+  border: 1px solid black;
+}
+.el-header {
+  // background-color: orange;
+  border: 1px solid black;
+}
+.el-footer {
+  // background-color: green;
+  border: 1px solid black;
+}
+.el-main {
+  // background-color: silver;
+  border: 1px solid black;
+  min-height: 700px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { router } from './router/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+// 5. 创建并挂载根实例
+const app = createApp(App)
+// 绑定路由
+app.use(router)
+// 绑定组件库
+app.use(ElementPlus)
+// 将vue实例挂载到Dom结构
+app.mount('#app')
